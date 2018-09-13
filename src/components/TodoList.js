b9e6e52f1bffbee206b46e59286293cb809ed121
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-09-13 16:42:02 +0700

    add delete user

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
+import Axios from "axios";
 
 class TodoList extends Component {
+  deleteUser = id => {
+    Axios.delete("http://localhost:3000/users/" + id).then(data => {
+      console.log(data);
+    });
+  };
+
   render() {
     return (
       <div>
         <h1>Todo List of: {this.props.name}</h1>
         <ul>
           {this.props.data.map(user => {
-            return <li>email: {user.email}</li>;
+            return (
+              <li>
+                <p>
+                  <button onClick={() => this.deleteUser(user.id)}>
+                    DELETE
+                  </button>{" "}
+                  email: {user.email}
+                </p>
+              </li>
+            );
           })}
         </ul>
         <button onClick={this.props.tambahTodo}>Add Todo</button>
